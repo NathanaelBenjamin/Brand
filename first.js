97c31f57133324f1df00c229669014ad83b68301
NathanaelBenjamin <86367360+NathanaelBenjamin@users.noreply.github.com>
2021-12-13 13:15:11 +0100

    Delete first.js

--- first.js
+++
@@ -1,9 +0,0 @@
-window.onload = function( ){
-    const btn =  document.querySelector(".togglebutton");
-    const logo = document.querySelector("#logo");
-    const navlinks = document.querySelector(".links");
-    
-btn.addEventListener('click', ( ) => {
-    navlinks.classList.toggle("toggle");
-    });
-}
